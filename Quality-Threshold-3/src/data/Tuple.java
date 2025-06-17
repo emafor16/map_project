6c325c1db556bbaa31bf5e71c9ac3cda9aa5b1f2
emafor16 <emanulele88@gmail.com>
2025-06-17 22:12:48 +0200

    Rimosso submodulo e aggiunta cartella come normale directory

---
+++ Quality-Threshold-3/src/data/Tuple.java
@@ -0,0 +1,68 @@
+package data;
+
+public class Tuple {
+
+    Item[] tuple;
+
+    // in input il numero di item che costruirà la tupla. costruisce l'oggetto riferito da tuple
+    public Tuple(int size) {
+        tuple = new Item[size];
+    }
+
+    public int getLength() {
+        return tuple.length;
+    }
+
+    public Item get(int i) {
+        if (i >= 0 && i < tuple.length) {
+            return tuple[i];
+        } else {
+            throw new IndexOutOfBoundsException("Indice fuori dal range della tupla.");
+        }
+    }
+
+    public void add(Item c, int i) {
+        if (i >= 0 && i < tuple.length) {
+            tuple[i] = c;
+        } else {
+            throw new IndexOutOfBoundsException("Indice fuori dal range della tupla.");
+        }
+    }
+
+    //determina la distanza tra la tupla riferita da obj e la tupla corrente (riferita da this). La distanza è ottenuta come la somma delle distanze tra gli item in posizioni eguali nelle due tuple. Fare uso di double distance(Object a) di Item
+    public double getDistance(Tuple obj) {
+        if (obj == null || obj.getLength() != this.getLength()) {
+            throw new IllegalArgumentException("Le tuple devono avere la stessa lunghezza e non possono essere null.");
+        }
+
+        double totalDistance = 0.0;
+
+        for (int i = 0; i < this.getLength(); i++) {
+            Item thisItem = this.get(i);
+            Item otherItem = obj.get(i);
+
+            if (thisItem == null || otherItem == null) {
+                throw new NullPointerException("Gli elementi delle tuple non possono essere null.");
+            }
+
+            totalDistance += thisItem.distance(otherItem.getValue());
+        }
+
+        return totalDistance;
+    }
+
+
+    //double avgDistance(Data data, int[] clusterData) restituisce la media delle distanze tra la tupla corrente e quelle ottenibili dalle righe della matrice data aventi indice in clusterData
+    public double avgDistance(Data data, int clusteredData[]){
+        double p=0.0;
+        double sumD=0.0;
+        for(int i=0;i<clusteredData.length;i++){
+            double d= getDistance(data.getItemSet(clusteredData[i]));
+            sumD+=d;
+        }
+        p=sumD/clusteredData.length;
+        return p;
+    }
+
+
+}
---
+++ Quality-Threshold-3/src/mining/ClusterSet.java
@@ -0,0 +1,39 @@
+package mining;
+
+import data.Data;
+
+public class ClusterSet {
+
+    private Cluster C[]= new Cluster[0];
+
+    public ClusterSet() {
+        // Initialize an empty cluster set
+        C = new Cluster[0];
+    }
+
+    public void add(Cluster c){
+        Cluster tempC[]=new Cluster[C.length+1];
+        for(int i=0;i<C.length;i++)
+            tempC[i]=C[i];
+        tempC[C.length]=c;
+        C=tempC;
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < C.length; i++) {
+            str.append("Cluster ").append(i).append(": ").append(C[i].toString()).append("\n");
+        }
+        return str.toString();
+    }
+
+    public String toString(Data data) {
+        String str="";
+        for(int i=0;i<C.length;i++){
+            if (C[i]!=null){
+                str+=i+":"+C[i].toString(data)+"\n";
+            }
+        }
+        return str;
+    }
+}
